6f2d7c0ecb774236eab76042e381f9a5b6d8074a
SebastianSlowakiewicz <14609@eppuz.edu.pl>
2025-04-15 20:31:45 +0200

    users app model and form (changed)

--- users/models.py
+++ users/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from phonenumber_field.modelfields import PhoneNumberField
 
 class Address(models.Model):
-    user = models.ForeignKey(User, related_name='user_address_set', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name='user_address_set', on_delete=models.RESTRICT)
     country = models.CharField(max_length=50)
     city = models.CharField(max_length=50)
     post_code = models.CharField(max_length=50)
@@ -16,16 +16,10 @@ class Users(User):
         ("dowod_osobisty", "Dowód osobisty"),
         ("prawo_jazdy", "Prawo jazdy"),
         ("paszport", "Paszport"),
-        ("legitymacja_studencka", "Legitymacja studencja"),
+        ("legitymacja_studencka", "Legitymacja studencka")
     ]
     phone = PhoneNumberField()
-    date_of_birth = models.DateField()
-    identity_document_type = models.CharField(max_length=50)
+    birth_date = models.DateField()
+    identity_document_type = models.CharField(max_length=50, choices=IDENTITY_DOCUMENT_TYPES)
     identity_document_no = models.CharField(max_length=50)
-    address = models.OneToOneField(Address, related_name='address', on_delete=models.CASCADE)
-
-
-
-
-
-   
+    address = models.OneToOneField(Address, related_name= 'user_address_set', on_delete=models.CASCADE)
--- users/views.py
+++ users/views.py
@@ -2,13 +2,12 @@ from django.shortcuts import render
 from . import forms
 
 def register(request):
-    from_user_register = forms.RegisterUserForm()
-    context = {'form_user_register': from_user_register}
-    return render(request, 'users/register.html.jinja')
+    form_users_register = forms.RegisterUserForm()
+    context = {'form_users_register': form_users_register}
+    return render(request, 'users/register.html.jinja', context=context)
 
 def login(request):
     return render(request, 'users/login.html.jinja')
-
 
 def logout(request):
     return render(request, 'users/logout.html.jinja')
