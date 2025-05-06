9f97ed78aa9f662e695879b7eaab387ba3d975c7
SebastianSlowakiewicz <14609@eppuz.edu.pl>
2025-05-06 15:40:37 +0200

    Added registration form

--- users/forms.py
+++ users/forms.py
@@ -1,3 +1,4 @@
+from dataclasses import fields
 from pyexpat import model
 from django.contrib.auth.forms import UserCreationForm
 from . import models
@@ -6,3 +7,4 @@ class RegisterUserForm(UserCreationForm):
     class Meta:
         model = models.Users
         exclude = ['id']
+        fields = ['username', 'email', 'password1', 'password2','first_name', 'last_name', 'phone', 'birth_date', 'identity_document_type', 'identity_document_no']
--- users/views.py
+++ users/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render
 from . import forms
 
 def register(request):
-    form_users_register = forms.RegisterUserForm()
-    context = {'form_users_register': form_users_register}
+    if request.method == 'post':
+        form_users_register = forms.RegisterUserForm(request.POST)
+        if form_users_register.is_valid():
+            form_users_register.save()
+            context = {'form_users_register': form_users_register}
+            return render(request, 'users/register.html.jinja', context=context)
+    else:
+        context = {'message': 'Zonk!'}
     return render(request, 'users/register.html.jinja', context=context)
 
 def login(request):
